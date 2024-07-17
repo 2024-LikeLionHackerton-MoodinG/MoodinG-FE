649d3c274e56b3e6602ebd3d34b5cd34c57f9fd0
indigo <diego97523@gmail.com>
2024-07-17 21:45:33 +0900

    [feat] /pages 파일 네이밍과 PhoneFrame ㄴ PhoneFrame.js는 아이폰 14ProMax에 맞춰서 제작함

--- src/index.js
+++ src/index.js
@@ -1,23 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { createBrowserRouter,RouterProvider } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LoadingPage from "./pages/LoadingPage.js";
+import ResultPage from "./pages/ResultPage.js";
+import StartPage from "./pages/StartPage.js";
+import WritePage from "./pages/WritePage.js";
 
 const router = createBrowserRouter([
   {
-    path:'/',
-    element:<App/>
-  }
-])
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/loading",
+    element: <LoadingPage />,
+  },
+  {
+    path: "/result/:id",
+    element: <ResultPage />,
+  },
+  {
+    path: "/start",
+    element: <StartPage />,
+  },
+  {
+    path: "/write",
+    element: <WritePage />,
+  },
+]);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(
-  <RouterProvider router={router}/>
-);
+root.render(<RouterProvider router={router} />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
---
+++ src/pages/LoadingPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PhoneFrame from '../common/PhoneFrame';
+
+const LoadingPage = () => {
+    return (
+        <PhoneFrame>
+            LoadingPage
+        </PhoneFrame>
+    );
+};
+
+export default LoadingPage;
