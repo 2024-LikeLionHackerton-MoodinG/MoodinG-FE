9904836801addaa7ac397c9c20a19febe34adae1
indigo <diego97523@gmail.com>
2024-07-22 22:19:01 +0900

    [feat] WritePage UI 구현 완

---
+++ src/lib/FontAwesome.js
@@ -0,0 +1,5 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
+import { faCircleInfo } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faChevronLeft, faCircleInfo);
