7cb1b03fb7e946efba62b5bb2df4958d11bff14e
ky <luminus.d3@gmail.com>
2024-07-22 17:50:08 +0900

    [chore] resut & start commponent 구분 및 이전 commit 반영 -result page 무딩 이미지 및 배경style 추가 -start page commponent 구분 -이전 commit style 반영하여 수정

---
+++ src/components/result/result.js
@@ -0,0 +1,18 @@
+import StyledResult from "./StyledRusilt";
+import resultMoodingImage from "../../lib/images/result-mooding.png";
+import StyledResultMoodingImage from "./StyledResultMoodingImage";
+
+const Result = () => {
+  return (
+    <>
+      <StyledResult>
+        <StyledResultMoodingImage
+          src={resultMoodingImage}
+          alt="result mooding img"
+        />
+      </StyledResult>
+    </>
+  );
+};
+
+export default Result;
--- src/components/start/Start.js
+++ src/components/start/Start.js
@@ -1,71 +1,22 @@
-import styled from "styled-components";
 import startMoodingImage from "../../lib/images/start-mooding.png";
+import StyledStartButton from "../start/StyledStartButton";
+import StyledStartMoodingImage from "./StyledStartMoodingImage";
+import StyledStart from "./StyledStart";
 
-const StyledStart = styled.div`
-  width: 430px;
-  height: 600px;
-  background: linear-gradient(#ffecb6, #fcf082);
-  position: relative;
-  overflow: hidden;
-
-  &::after {
-    content: "";
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    width: 100%;
-    height: 50px;
-    background: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 430 100"><path d="M0,80 Q107.5,40 215,80 T430,80 L430,100 L0,100 Z" fill="white"/></svg>')
-      no-repeat bottom;
-    background-size: cover;
-  }
-`;
-
-const StartmoodingImage = styled.img`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-`;
-
-const StartButton = styled.button`
-  position: absolute;
-  bottom: 100px;
-  left: 50%;
-  transform: translateX(-50%);
-  width: 350px;
-  height: 70px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-radius : 20px;
-  background-color: white;
-  cursor: pointer;
-  &:active{
-    border-width:4px;
-    background-color: #f4f1f1;
-    opacity: 75%;
-  }
-  box-shadow: 2px 2px 2px;
-`;
-
-const StartButtonText = styled.span`
-  position: absolute;
-  color: black;
-  font-size: 35px;
-  font-weight: bold;
-  pointer-events: none;
-`;
+const startClick = () => {
+  console.log("start");
+};
 
 const Start = () => {
   return (
     <>
       <StyledStart>
-        <StartmoodingImage src={startMoodingImage} alt="start mooding img" />
+        <StyledStartMoodingImage
+          src={startMoodingImage}
+          alt="start mooding img"
+        />
       </StyledStart>
-      <StartButton>
-        <StartButtonText>시작 하기</StartButtonText>
-      </StartButton>
+      <StyledStartButton onClick={startClick}>시작하기</StyledStartButton>
     </>
   );
 };
