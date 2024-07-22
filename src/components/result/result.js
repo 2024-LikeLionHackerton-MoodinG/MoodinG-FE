b14de18e0032924b0dbb9b31b0631d86cd8e9a76
indigo <diego97523@gmail.com>
2024-07-23 00:23:50 +0900

    [fix] Components/Result 파일들 하나로..

--- src/components/result/result.js
+++ src/components/result/result.js
@@ -1,10 +1,29 @@
-import StyledResult from "./StyledRusult";
+import { GlobalFontDNF } from "../../lib/fontSetting";
+import styled from "styled-components";
 import resultMoodingImage from "../../lib/images/result-mooding.png";
-import StyledResultMoodingImage from "./StyledResultMoodingImage";
+
+
+const StyledResult = styled.div`
+  width: 430px;
+  height: 932px;
+  background: linear-gradient(#ffecb6, #fcf082);
+  position: relative;
+  overflow: hidden;
+`;
+
+
+const StyledResultMoodingImage = styled.img`
+  position: absolute;
+  bottom: 300px;
+  left: 45%;
+  transform: translateX(-50%);
+  width: 200px;
+`;
 
 const Result = () => {
   return (
     <>
+      <GlobalFontDNF />
       <StyledResult>
         <StyledResultMoodingImage
           src={resultMoodingImage}
--- src/pages/ResultPage.js
+++ src/pages/ResultPage.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import PhoneFrame from '../components/common/PhoneFrame.js'
+import Result from '../components/result/Result.js'
 
 const ResultPage = () => {
     return (
         <PhoneFrame>
-            ResultPage
+            <Result/>
         </PhoneFrame>
     );
 };
