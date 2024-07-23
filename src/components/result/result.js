a0a53c925e5a7beee23c0f2b38d56453deb0c94f
ky <luminus.d3@gmail.com>
2024-07-23 17:02:19 +0900

    [feat] result page UI구현 - start page 파일 하나로 묶음

--- src/components/result/result.js
+++ src/components/result/result.js
@@ -1,23 +1,58 @@
 import { GlobalFontDNF } from "../../lib/fontSetting";
 import styled from "styled-components";
 import resultMoodingImage from "../../lib/images/result-mooding.png";
+import answerballon from "../../lib/images/answerballon.png";
 import { Wave } from "../common/Wave";
 
-const StyledResultContainer = styled.div`
+const ResultContainer = styled.div`
   width: 430px;
   height: 932px;
   background: linear-gradient(#ffecb6, #fcf082);
-  position: relative;
   overflow: hidden;
 `;
 
 // StyledResultMoodingImage 컴포넌트 정의
-const StyledResultMoodingImage = styled.img`
-  position: absolute;
-  bottom: 300px;
-  left: 45%;
-  transform: translateX(-50%);
-  width: 200px;
+const ResultMoodingImage = styled.img`
+  position: relative;
+  transform: translate(65px, 50px);
+  width: 300px;
+`;
+
+const AnswerBallon = styled.img`
+  position: relative;
+  transform: translate(15px, 30px);
+  width: 400px;
+`;
+
+const ButtonBar = styled.div`
+  position: relative;
+  top: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 430px;
+  height: 80px;
+  gap: 50px;
+`;
+
+const Button = styled.button`
+  color: white;
+  border: 3px solid black;
+  font-size: 25px;
+  background-color: black;
+  font-family: "DNFBitBitv2", sans-serif;
+  border-radius: 20px;
+  height: 50px;
+  width: 150px;
+  cursor: pointer;
+  transition: background-color 0.1s;
+
+  &:active {
+    border-width: 4px;
+    background-color: #fafaa0;
+    opacity: 75%;
+  }
+  box-shadow: 2px 2px 2px;
 `;
 
 // Result 컴포넌트 정의
@@ -25,12 +60,19 @@ const Result = () => {
   return (
     <>
       <GlobalFontDNF />
-      <StyledResultContainer>
-        <StyledResultMoodingImage
+
+      <ResultContainer>
+        <AnswerBallon src={answerballon} alt="answer ballon"></AnswerBallon>
+        <ResultMoodingImage
           src={resultMoodingImage}
           alt="result mooding img"
         />
-      </StyledResultContainer>
+
+        <ButtonBar>
+          <Button>1</Button>
+          <Button>2</Button>
+        </ButtonBar>
+      </ResultContainer>
     </>
   );
 };
