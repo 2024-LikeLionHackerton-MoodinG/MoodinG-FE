75d9e2dcd4d7f58122a6a6fd2a3c3295e0ca6e77
indigo <diego97523@gmail.com>
2024-07-24 18:38:12 +0900

    [fix] StartPage 구현 ㄴTypeIt 라이브러리를 통한 글씨 애니메이션 추가 ㄴUI 변경

--- src/components/start/Start.js
+++ src/components/start/Start.js
@@ -2,56 +2,80 @@ import styled from "styled-components";
 import startMoodingImage from "../../lib/images/start-mooding.png";
 import { GlobalFontDNF } from "../../lib/fontSetting";
 import { Wave } from "../common/Wave";
+import trashcan from "../../lib/images/Mooding_trashcan.png";
+import StartButton from "./StartButton";
+import { useState } from "react";
+import StartExplain from "./StartExplain";
+
+const StartContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 const StyledStart = styled(Wave)`
   width: 430px;
   height: 600px;
-`;
-
-const StyledStartButton = styled.button`
-  position: absolute;
-  bottom: 150px;
-  color: black;
-  border: 3px solid black;
-  font-size: 25px;
-  background-color: white;
-  font-family: "DNFBitBitv2", sans-serif;
-  border-radius: 20px;
-  min-height: 70px;
-  min-width: 340px;
-  cursor: pointer;
-  transition: background-color 0.1s;
-  &:active {
-    border-width: 4px;
-    background-color: #fafaa0;
-    opacity: 75%;
-  }
-  box-shadow: 2px 2px 2px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
 
 const StyledStartMoodingImage = styled.img`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
+  width: 240px;
+  height: 285px;
 `;
 
-const startClick = () => {
-  console.log("start");
-};
+const SubExplainDiv = styled.div`
+  font-family: "DNFBitBitv2", sans-serif;
+  font-size: 20px;
+  background: linear-gradient(#fcc101, #f26804);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-clip: text;
+  color: transparent;
+`;
+
+const TrashCanContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const TrashCanImg = styled.img`
+  width: 45px;
+  height: 65px;
+  margin-right: 10px;
+`;
+
+const MoodingWord = styled.div`
+  font-family: "DNFBitBitv2", sans-serif;
+  font-size: 36px;
+`;
 
 const Start = () => {
+  const [buttonClick, setButtonClick] = useState(false);
   return (
-    <>
+    <StartContainer>
       <GlobalFontDNF />
       <StyledStart>
         <StyledStartMoodingImage
           src={startMoodingImage}
           alt="start mooding img"
         />
+        <SubExplainDiv>나만의 감정 쓰레기통</SubExplainDiv>
+        <TrashCanContainer>
+          <TrashCanImg src={trashcan} />
+          <MoodingWord>무딩</MoodingWord>
+        </TrashCanContainer>
       </StyledStart>
-      <StyledStartButton onClick={startClick}>시작하기</StyledStartButton>
-    </>
+      {buttonClick ? (
+        <StartExplain buttonClick={buttonClick} setButtonClick={setButtonClick}>
+
+        </StartExplain>
+      ) : (
+        <StartButton setButtonClick={setButtonClick}>시작하기</StartButton>
+      )}
+    </StartContainer>
   );
 };
 
---
+++ src/components/start/StartButton.js
@@ -0,0 +1,70 @@
+import styled from "styled-components";
+import smallMooding2 from "../../lib/images/Mooding_small2.png";
+
+const ButtonsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 333px;
+`;
+const StyledStartButton = styled.button`
+  margin-top: 100px;
+  bottom: 150px;
+  color: black;
+  border: 3px solid black;
+  font-size: 25px;
+  background-color: white;
+  font-family: "DNFBitBitv2", sans-serif;
+  border-radius: 20px;
+  min-height: 70px;
+  min-width: 340px;
+  cursor: pointer;
+  transition: background-color 0.1s;
+  &:active {
+    border-width: 4px;
+    background-color: #fafaa0;
+    opacity: 75%;
+  }
+  box-shadow: 2px 2px 2px;
+`;
+
+const InformationButtonContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-top: 30px;
+`;
+
+const InformationImg = styled.img`
+  width: 40px;
+  height: 34.47px;
+`;
+const InformationButton = styled.button`
+  background-color: transparent;
+  font-family: "DNFBitBitv2", sans-serif;
+  font-size: 16px;
+  border-width: 0;
+  cursor: pointer;
+  &:hover {
+    text-decoration: underline 3px black;
+  }
+`;
+
+const StartButton = ({ setButtonClick }) => {
+  return (
+    <ButtonsContainer>
+      <StyledStartButton
+        onClick={() => {
+          setButtonClick(true);
+        }}
+      >
+        시작하기
+      </StyledStartButton>
+      <InformationButtonContainer>
+        <InformationImg src={smallMooding2}></InformationImg>
+        <InformationButton>알아보기</InformationButton>
+      </InformationButtonContainer>
+    </ButtonsContainer>
+  );
+};
+
+export default StartButton;
