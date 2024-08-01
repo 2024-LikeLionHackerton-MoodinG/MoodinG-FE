9570fc03d7bba176612f857dea1169f0c3b7bcab
BeomSu Kim <33516980+Indigochi1d@users.noreply.github.com>
2024-08-01 17:44:45 +0900

    [style] Responsive Width

--- src/components/common/PhoneFrame.js
+++ src/components/common/PhoneFrame.js
@@ -2,16 +2,17 @@ import styled from 'styled-components';
 
 const StyledPhoneFrame = styled.div`
     width: 430px;
-    height: 99.75vh;
+    height: 99.75dvh;
     display: flex;
     justify-content: center;
     border: solid 0.5px #dfdfe0;
     overflow-y: hidden;
+    overflow-x: hidden;
 
     /* 미디어 쿼리 - 휴대폰) */
-    @media (max-width: 480px) {
-        width: 95vw;
-        height:100vh;
+    @media (max-width: 430px) {
+        width: 100dvw;
+        height:100dvh;
         border: none;
     }
 `;
