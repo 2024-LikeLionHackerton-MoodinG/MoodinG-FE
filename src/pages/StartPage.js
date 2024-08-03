a380d7ea35d1595a323d9f220d6238398c7ad17b
indigo <diego97523@gmail.com>
2024-08-03 22:22:02 +0900

    [fix] startPage 주석  삭제

--- src/pages/StartPage.js
+++ src/pages/StartPage.js
@@ -1,18 +1,13 @@
-import React from 'react';
-import PhoneFrame from '../components/common/PhoneFrame';
-import Start from '../components/start/Start';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import PhoneFrame from "../components/common/PhoneFrame";
+import Start from "../components/start/Start";
 
 const StartPage = () => {
-    const navigate = useNavigate();
-    return (
-        <PhoneFrame>
-            {/* <Start /> */}
-            <button onClick={()=>{
-                navigate("/result/1");
-            }}>1</button>
-        </PhoneFrame>
-    );
+  return (
+    <PhoneFrame>
+      <Start />
+    </PhoneFrame>
+  );
 };
 
 export default StartPage;
